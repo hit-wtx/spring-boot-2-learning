fac5fafc96baf4aa25f2017c81038c0c0e69b560
wentuanxu <wentuanxu@meituan.com>
2018-11-04 21:57:35 +0800

    spring boot2 mybatis HikariCP 连接池

---
+++ springboot2-mybatis-hikaricp/src/main/java/com/lib/springboot2/service/UserService.java
@@ -0,0 +1,19 @@
+package com.lib.springboot2.service;
+
+import com.lib.springboot2.domain.UserDomain;
+
+import java.util.List;
+
+/**
+ * @Author wtx
+ * @Date 2018/11/4 下午9:48
+ */
+public interface UserService {
+    int insert(UserDomain record);
+
+    void deleteUserById(Integer userId);
+
+    void updateUser(UserDomain userDomain);
+
+    List<UserDomain> selectUsers();
+}
